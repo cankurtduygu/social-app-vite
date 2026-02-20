f75df297b25dee621e0c3905a0ff3d3bfbb09ddc
Duygu cankurt <dyg.cankurt17@gmail.com>
2026-02-20 11:41:49 +0100

    add init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
+import Rightbar from './components/Rightbar';
+import Box from '@mui/material/Box';
+import { Stack } from '@mui/material';
+
+function App() {
+  return (
+    <Box>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Rightbar = () => {
+  return (
+    <Box bgcolor='pink' flex={2} p={2}>Rightbar</Box>
+  )
+}
+
+export default Rightbar
